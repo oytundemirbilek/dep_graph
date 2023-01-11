b2264a41ece07d00d72221267840ecf09bb19746
oytundemirbilek <oytun1996@gmail.com>
2023-01-11 02:03:56 +0100

    command line arguments

--- dep_graph/__init__.py
+++ dep_graph/__init__.py
@@ -1,7 +1,13 @@
+import argparse
 from dep_graph.dep_graph import DependencyHandler
 
 
 def main():
-    handler = DependencyHandler()
+    parser = argparse.ArgumentParser(
+        description="dep_graph prints out the dependency graph of a package."
+    )
+    parser.add_argument("-f", "--filepath", help="File path to a dependency json file.")
+    args = parser.parse_args()
+    handler = DependencyHandler(args.filepath)
     handler.load_dependencies()
     handler.print_dependencies()
--- dep_graph/dep_graph.py
+++ dep_graph/dep_graph.py
@@ -4,12 +4,25 @@ from typing import Dict
 
 
 class DependencyHandler:
-    def __init__(self, dep_filename: str):
-        self.dep_filename = dep_filename
+    def __init__(self, dep_filepath: str):
+        """
+        Constructor for the DependencyHandler class requires a file path to a json file.
+
+        Parameters
+        ----------
+        dep_filepath: str
+            File path to the dependency json file.
+
+        """
+        self.dep_filepath = dep_filepath
         self.dependency_dict: Dict[str, list] = {}
+        # Tree level is only used for string formatting when printing.
         self.tree_level = 0
 
     def _print_single_package_dependencies(self, package_name: str) -> None:
+        """
+        Formats and prints the provided dependency data.
+        """
         self.tree_level += 1
         for dependency in self.dependency_dict[package_name]:
             print(self.tree_level * 2 * " " + f"- {dependency}")
@@ -35,11 +48,11 @@ class DependencyHandler:
             A dictionary where each key is a package name and its value is a list of dependency names.
         """
         try:
-            with open(self.dep_filename) as dep_file:
+            with open(self.dep_filepath) as dep_file:
                 dependency_dict = json.load(dep_file)
             self.dependency_dict = dependency_dict
         except Exception as e:
-            logging.warning("Dependency list cannot be loaded.")
+            logging.warning("Dependency list could not be loaded.")
             logging.warning(e)
 
         return self.dependency_dict
